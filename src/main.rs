3ff6f022a248e59ab0f94887180c04acbdc4b3dc
Energyboy-OwO <94549302+Energyboy-OwO@users.noreply.github.com>
2026-07-17 08:37:12 +0100

    bleh added latency, brain gone, code yoinked from example codes

--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,12 @@
 
 // imports :3
 use serenity::async_trait;
+use serenity::builder::EditMessage; // Required for Serenity v0.12+
 use serenity::model::channel::Message;
 use serenity::model::gateway::Ready;
 use serenity::prelude::*;
 use std::env;
+use std::time::Instant;
 
 struct Handler;
 
@@ -14,11 +16,29 @@ struct Handler;
 // implementation of event handler so when message contents == .ping we get a pong :3
 impl EventHandler for Handler {
     async fn message(&self, ctx: Context, msg: Message) {
-        if msg.content == ".ping"
-            && let Err(why) = msg.channel_id.say(&ctx.http, "Pong :3").await
-        {
-            println!("Error sending message: {why:?}");
+        if msg.content == ".ping" {
+            // start the timer
+            let start_time = Instant::now();
+
+            // initial message :3
+            match msg.channel_id.say(&ctx.http, "Pinging.... :3").await {
+                Ok(mut response_msg) => {
+                    // calculate the time
+                    let latency = start_time.elapsed().as_millis();
+                    let new_content = format!("Pong! Latency is **{}ms** :3", latency);
+
+                    let builder = EditMessage::new().content(new_content);
+
+                    if let Err(why) = response_msg.edit(&ctx.http, builder).await {
+                        println!("Error editing the message: {why:?}");
+                    }
+                }
+                Err(why) => {
+                    println!("Error sending message: {why:?}");
+                }
+            }
         }
+
         if msg.content == ".coinflip" {
             let flip = rand::random();
             let result = if flip { "Heads :3" } else { "Tails :3" };
@@ -36,17 +56,19 @@ async fn main() {
     // config client with the discord bot token in the .env
     dotenv::dotenv().ok();
     let token = env::var("DISCORD_TOKEN").expect("expected a token in the .env");
+
     // set gateway intents or big discord won't be happy
     let intents = GatewayIntents::GUILD_MESSAGES
         | GatewayIntents::DIRECT_MESSAGES
         | GatewayIntents::MESSAGE_CONTENT;
+
     // create new instance of client logging in as the bot
     let mut client = Client::builder(&token, intents)
         .event_handler(Handler)
         .await
         .expect("Error creating a client");
 
-    // finafuckingly start a single shard (whatever that means) and start listening to events :3
+    // finafuckingly start a single shard and start listening to events :3
     if let Err(why) = client.start().await {
         println!("client error: {why:?}");
     }
